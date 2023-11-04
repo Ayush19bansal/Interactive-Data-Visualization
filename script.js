96e957b9a792592ddb31bc6451b57d62056d16b1
Ayush19bansal <bansalaashu7@gmail.com>
2023-11-04 22:10:27 +0530

    added

---
+++ script.js
@@ -0,0 +1,52 @@
+
+let data = {
+    labels: ['January', 'February', 'March', 'April', 'May'],
+    datasets: [{
+        label: 'Monthly Sales',
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+        data: [65, 59, 80, 81, 56],
+    }],
+};
+
+
+let figure = {
+    type: 'bar',
+    data: data,
+    options: {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Monthly Sales Data',
+            },
+        },
+    },
+};
+
+// Create the chart
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, figure);
+
+// Add interactivity (e.g., tooltip) using Chart.js built-in features
+myChart.options.plugins.tooltip = {
+    callbacks: {
+        label: function (context) {
+            return `Sales: ${context.parsed.y}`;
+        },
+    },
+};
+
+// Apply smooth animations using Anime.js
+const chartAnimation = anime({
+    targets: myChart.data.datasets[0].data,
+    easing: 'linear',
+    delay: anime.stagger(200),
+    duration: 1000,
+    loop: true,
+    direction: 'alternate',
+    update: function (anim) {
+        myChart.update();
+    },
+});
